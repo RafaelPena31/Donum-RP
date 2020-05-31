20648deef62571bc375738324676a53c427a8db7
Rafael Augusto Pena <58792212+RafaelPena31@users.noreply.github.com>
2020-05-31 10:04:57 -0300

    go

---
+++ src/component/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+const Header = () => (
+    <header id={"main-header"}>
+        <nav>
+            <a href={"#"}> Home </a>
+            <a href={"#"}> Home </a>
+            <a href={"#"}> Home </a>
+        </nav>
+        <h1>Donum</h1>
+    </header>
+);
+
+export default Header;
